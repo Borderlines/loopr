e7c38edbf5b66af7a5573a74b47bccea6a5c8e8b
Edouard <edou4rd@gmail.com>
2015-06-22 23:05:13 +0200

    :postal_horn:

--- app.py
+++ app.py
@@ -6,6 +6,25 @@ from flask import render_template
 
 app = Eve(__name__, template_folder='templates')
 assets = Environment(app)
+app.register_resource('shows', {
+    # most global settings can be overridden at resource level
+    'resource_methods': ['GET', 'POST'],
+    'schema': {
+        # Schema definition, based on Cerberus grammar. Check the Cerberus project
+        # (https://github.com/nicolaiarocci/cerberus) for details.
+        'title': {
+            'required': True,
+            'type': 'string'
+        },
+        'description': {
+            'type': 'string'
+        },
+        'type': {
+            'type': 'list',
+            'allowed': ['TweetShow', 'YoutubeShow']
+        }
+    }
+})
 
 
 @app.route('/')
@@ -13,4 +32,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+    debug = True
+    app.run(debug=debug, use_reloader=debug)
--- settings.py
+++ settings.py
@@ -2,8 +2,10 @@
 # MONGO_PORT = 27017
 # MONGO_USERNAME = 'user'
 # MONGO_PASSWORD = 'user'
-# MONGO_DBNAME = 'apitest'
-DOMAIN = {'people': {}}
-URL_PREFIX = 'api'
+MONGO_DBNAME = 'loopr-dev'
 DEBUG = True
+URL_PREFIX = 'api'
 LESS_BIN = 'node_modules/.bin/lessc'
+RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
+ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
+DOMAIN = {}
