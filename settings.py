c1279e584f854fee9747827ae3e6d63dcbfbf631
Edouard <edou4rd@gmail.com>
2015-06-30 17:29:44 +0200

    MONGOLAB_URI in settings

--- settings.py
+++ settings.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+import os
+
 # MONGO_HOST = 'localhost'
 # MONGO_PORT = 27017
 # MONGO_USERNAME = 'user'
-# MONGO_PASSWORD = 'user'
+# MONGO_PASSWORD = 'user
 MONGO_DBNAME = 'loopr-dev'
+if os.environ.get('MONGOLAB_URI'):
+    MONGO_URI = os.environ.get('MONGOLAB_URI')
 DEBUG = True
 URL_PREFIX = 'api'
 LESS_BIN = '/home/edouard/work/loopr/node_modules/.bin/lessc'
