a33e58f66b9813b49725f654f99a96df8714fb01
Edouard <edou4rd@gmail.com>
2015-07-08 15:14:43 +0200

    direct loop

--- static/loopr/player/player.ctrl.js
+++ static/loopr/player/player.ctrl.js
@@ -14,22 +14,55 @@
                 autoplay: 1,
                 showinfo: 0
             },
-            playShow: function(show) {
+            playShow: function(show, index) {
+                if (!angular.isDefined(show)) {
+
+                    var now = new Date(),
+                    then = new Date(
+                        now.getFullYear(),
+                        now.getMonth(),
+                        now.getDate(),
+                        0,0,0),
+                    diff = (now.getTime() - then.getTime()) / 1000;
+                    var watch_at = diff % vm.loop.duration();
+                    var spent = 0;
+                    for (var i = 0; i < vm.loop.shows.length; i++) {
+                        if (angular.isDefined(index)) {
+                            break;
+                        }
+                        show = vm.loop.shows[i];
+                        if (spent < watch_at) {
+                            if (spent + show.duration() < watch_at) {
+                                spent += show.duration();
+                            } else {
+                                for (var j = 0; j < show.links.length; j++) {
+                                    var link = show.links[j];
+                                    if (spent + link.duration < watch_at) {
+                                        spent += link.duration;
+                                    } else {
+                                        index = j;
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                index = index || 0;
                 vm.currentShow = show;
+                vm.currentItem = show.links[index];
                 if (show.type === 'VideoShow') {
-                    vm.playVideoShow(show);
+                    vm.playVideoShow(show, index);
                 }
             },
             playVideoShow: function(show, index) {
-                index = index || 0;
-                vm.currentItem = show.links[index];
-                vm.youtubeUrl = vm.currentItem.url;
+                vm.youtubeUrl = show.links[index].url;
             },
-            nextInVideoShow: function() {
+            nextItem: function() {
                 var current_item_index = vm.currentShow.links.indexOf(vm.currentItem);
                 if (vm.currentShow.links.length - 1 > current_item_index) {
                     // next item
-                    vm.playVideoShow(vm.currentShow, current_item_index + 1);
+                    vm.playShow(vm.currentShow, current_item_index + 1);
                 } else {
                     // next show
                     vm.nextShow();
@@ -50,14 +83,14 @@
         Accounts.one($routeParams.username).get().then(function(user) {
             Loops.getList({where: {user_id: user._id}, embedded:{shows:1}}).then(function(loop) {
                 vm.loop = loop[0];
-                vm.playShow(vm.loop.shows[0]);
+                vm.playShow();
             });
         });
         $rootScope.$on('youtube.player.error', function ($event, player) {
-            vm.nextInVideoShow();
+            vm.nextItem();
         });
         $rootScope.$on('youtube.player.ended', function ($event, player) {
-            vm.nextInVideoShow();
+            vm.nextItem();
         });
 
     }
