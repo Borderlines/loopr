1a4906b2579211a571593792e1fb6ae3519a8308
Edouard <edou4rd@gmail.com>
2016-03-06 22:58:09 +0100

    get duration

--- app/l8pr/models.py
+++ app/l8pr/models.py
@@ -1,7 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.db.models import signals
+import urllib
+import datetime
+import re
+from django.conf import settings
 # TODO: favorites in accounts
+
+SHOW_TYPES = (('MusicShow', 'MusicShow'), ('VideoShow', 'VideoShow'))
+PROVIDER_CHOICES = (('YouTube', 'YouTube'), ('SoundCloud', 'SoundCloud'))
 
 
 class Loop(models.Model):
@@ -23,8 +30,6 @@ class ShowsRelationship(models.Model):
     def __str__(self):
         return '%s ===> %s' % (self.show, self.loop)
 
-SHOW_TYPES = (('MusicShow', 'MusicShow'), ('VideoShow', 'VideoShow'))
-
 
 class Show(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -40,15 +45,16 @@ class Show(models.Model):
         return self.title
 
 
+
 class Item(models.Model):
     show = models.ForeignKey(Show, related_name='items', on_delete=models.SET_NULL, null=True)
     # meta
-    title = models.CharField(max_length=255)
-    author_name = models.CharField(max_length=255, null=True)
-    thumbnail = models.URLField(max_length=200, null=True)
-    provider_name = models.CharField(max_length=255, null=True)
-    html = models.TextField(null=True)
-    duration = models.PositiveIntegerField()
+    title = models.CharField(max_length=255, null=True, blank=True)
+    author_name = models.CharField(max_length=255, null=True, blank=True)
+    thumbnail = models.URLField(max_length=200, null=True, blank=True)
+    provider_name = models.CharField(max_length=255, choices=PROVIDER_CHOICES)
+    html = models.TextField(null=True, blank=True)
+    duration = models.PositiveIntegerField(null=True, blank=True)
     url = models.URLField(max_length=200)
 
     class Meta:
@@ -56,3 +62,66 @@ class Item(models.Model):
 
     def __str__(self):
         return self.url
+
+
+def get_youtube_duration(url):
+    from apiclient.discovery import build
+
+    def video_id(value):
+        """
+        Examples:
+        - http://youtu.be/SA2iWivDJiE
+        - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+        - http://www.youtube.com/embed/SA2iWivDJiE
+        - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+        """
+        query = urllib.parse.urlparse(value)
+        if query.hostname == 'youtu.be':
+            return query.path[1:]
+        if query.hostname in ('www.youtube.com', 'youtube.com'):
+            if query.path == '/watch':
+                p = urllib.parse.parse_qs(query.query)
+                return p['v'][0]
+            if query.path[:7] == '/embed/':
+                return query.path.split('/')[2]
+            if query.path[:3] == '/v/':
+                return query.path.split('/')[2]
+        return None
+
+    youtube = build('youtube', 'v3', developerKey=getattr(settings, 'GOOGLE_API_KEY'))
+    search_response = youtube.videos().list(
+        part='contentDetails',
+        id=video_id(url),
+        maxResults=1
+    ).execute()
+    result = search_response.get("items", [])
+    duration = re.split('\D', result[0].get('contentDetails').get('duration'))
+    duration = [int(d) for d in duration if d != '']
+    if len(duration) == 1:
+        duration = datetime.timedelta(seconds=duration[0])
+    elif len(duration) == 2:
+        duration = datetime.timedelta(minutes=duration[0], seconds=duration[1])
+    elif len(duration) == 3:
+        duration = datetime.timedelta(hours=duration[0], minutes=duration[1], seconds=duration[2])
+    return duration.seconds
+
+
+def get_soundcloud_duration(url):
+    import soundcloud
+    from requests import HTTPError
+    client = soundcloud.Client(client_id='847e61a8117730d6b30098cfb715608c')
+    try:
+        return round(client.get('/resolve', url=url).duration / 1000)
+    except HTTPError:
+        return None
+
+
+def completeItem(sender, instance, created, **kwargs):
+    if not instance.duration:
+        if instance.provider_name == 'YouTube':
+            instance.duration = get_youtube_duration(instance.url)
+        if instance.provider_name == 'SoundCloud':
+            instance.duration = get_soundcloud_duration(instance.url)
+        instance.save()
+
+signals.post_save.connect(completeItem, sender=Item)
