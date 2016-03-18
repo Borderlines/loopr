cdc7436acde1544adb8aaf84dca1fe8baeacf903
Edouard <edou4rd@gmail.com>
2016-03-18 12:26:28 +0100

    migrations merged

--- app/l8pr/models.py
+++ app/l8pr/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import signals
-from awesome_avatar.fields import AvatarField
 from django.conf import settings
 import urllib
 import datetime
