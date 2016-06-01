210636995f02f75ed41354ec992c1c301b7384d3
Benetco <benjamin.etco@gmail.com>
2016-06-01 11:59:15 +0200

    Values tuning

--- app/settings.py
+++ app/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
 HAYSTACK_CONNECTIONS = {
     'default': {
         'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-        'URL': 'http://172.17.0.2:9200/',
+        'URL': 'http://127.0.0.1:9200/',
         'INDEX_NAME': 'haystack',
     },
 }
