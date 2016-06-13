1d2e3c81d3ffc511fc15d6e2e5e4a270b12367ba
Edouard <edou4rd@gmail.com>
2016-06-13 18:38:25 +0200

    init webtorrent

--- app/l8pr/static/player/webtorrent/module.js
+++ app/l8pr/static/player/webtorrent/module.js
@@ -8,51 +8,71 @@
             },
             restrict: 'E',
             link: function(scope, element) {
-                var client = new $window.WebTorrent();
-                client.add(scope.item.url, function (torrent) {
-                    $interval(function() {
+                var client, player, interval;
+                var playerEvents = {
+                        error: function(event) {
+                            Player.setCurrentPosition(0);
+                            Player.nextItem();
+                        },
+                        playing: function(event) {
+                            Player.setStatus('playing');
+                            Player.volume = (player.muted) ? 0 : 1;
+                        },
+                        timeupdate: function(event) {
+                            Player.setCurrentPosition((player.currentTime / player.duration) * 100);
+                        },
+                        pause: function(event) {
+                            Player.setStatus('pause');
+                        },
+                        ended: function(event) {
+                            Player.setCurrentPosition(0);
+                            Player.nextItem();
+                        }
+                    };
+                scope.$watch('item', function() {
+                    init();
+                });
+                function init() {
+                    $interval.cancel(interval);
+                    if (client) {
+                        client.destroy();
                         angular.extend(scope, {
-                            torrent: torrent
-                            // progress: torrent.progress
+                            torrent: {}
                         });
-                    }, 1000);
-                    var file = torrent.files[0];
-                    file.appendTo(element.get(0), function ready(err) {
-                        var player = element.find('video').get(0);
-                        // var progressionTracker;
-                        scope.$on('player.playPause', function() {
-                            if (player.paused) {
-                                player.play();
-                                } else {
-                                player.pause();
-                            }
+                        client = null;
+                    }
+                    if (player) {
+                        angular.forEach(playerEvents, function(eventHandler, eventName) {
+                            player.removeEventListener(eventName, eventHandler);
                         });
-                        scope.$on('player.seekTo', function(e, percent) {
-                            player.currentTime = (percent/100) * player.duration;
-                        });
-                        scope.$on('player.toggleMute', function() {
-                            player.volume = (Player.isMuted) ? 0 : 1;
-                        });
-                        var playerEvents = {
-                                error: function(event) {
-                                    Player.setCurrentPosition(0);
-                                    Player.nextItem();
-                                },
-                                playing: function(event) {
-                                    Player.setStatus('playing');
-                                    Player.volume = (player.muted) ? 0 : 1;
-                                },
-                                timeupdate: function(event) {
-                                    Player.setCurrentPosition((player.currentTime / player.duration) * 100);
-                                },
-                                pause: function(event) {
-                                    Player.setStatus('pause');
-                                },
-                                ended: function(event) {
-                                    Player.setCurrentPosition(0);
-                                    Player.nextItem();
+                        player = null;
+                    }
+                    element.find('video').remove();
+                    client = new $window.WebTorrent();
+                    client.add(scope.item.url, function (torrent) {
+                        interval = $interval(function() {
+                            angular.extend(scope, {
+                                torrent: torrent
+                                // progress: torrent.progress
+                            });
+                        }, 1000);
+                        var file = torrent.files[0];
+                        file.appendTo(element.get(0), function ready(err) {
+                            player = element.find('video').get(0);
+                            // var progressionTracker;
+                            scope.$on('player.playPause', function() {
+                                if (player.paused) {
+                                    player.play();
+                                    } else {
+                                    player.pause();
                                 }
-                            };
+                            });
+                            scope.$on('player.seekTo', function(e, percent) {
+                                player.currentTime = (percent/100) * player.duration;
+                            });
+                            scope.$on('player.toggleMute', function() {
+                                player.volume = (Player.isMuted) ? 0 : 1;
+                            });
                             angular.forEach(playerEvents, function(eventHandler, eventName) {
                                 player.addEventListener(eventName, eventHandler);
                             });
@@ -61,9 +81,9 @@
                                     player.removeEventListener(eventName, eventHandler);
                                 });
                             });
-
+                        });
                     });
-                });
+                }
             },
             template: [
                 '<div class="webtorrent__info">',
