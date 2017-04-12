d624bc1a39d3840dabd6e9b0fb4baf0ec98b68c6
Edouard Richard <edou4rd@gmail.com>
2017-04-12 18:44:40 +0200

    settings basic

--- app/settings.py
+++ app/settings.py
@@ -83,17 +83,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'loopr',
-        'USER': 'postgres',
-        'PASSWORD': 'mysecretpassword',
-        'HOST': '172.17.0.2',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
