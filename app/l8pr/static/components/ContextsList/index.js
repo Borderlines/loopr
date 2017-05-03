b8439b2c51e0f1f1cf93216651d9ac9bff38a265
Edouard Richard <edou4rd@gmail.com>
2017-05-03 15:23:17 +0200

    update height list items

--- app/l8pr/static/components/ContextsList/index.js
+++ app/l8pr/static/components/ContextsList/index.js
@@ -5,11 +5,13 @@ import { AutoSizer, List } from 'react-virtualized'
 import './style.scss'
 
 
-export default function ContextsList({ contexts, onItemPlayClick, currentItem, onPlayShowClick, onAddClick }) {
-    function getContextHeight({ index }) {
+export default class ContextsList extends React.Component {
+    getContextHeight({ index }) {
+        const { contexts } = this.props
         return contexts[index].items.length * 100
     }
-    function _rowRenderer({ index, key, style }) {
+    _rowRenderer({ index, key, style }) {
+        const { contexts, onItemPlayClick, onPlayShowClick, onAddClick } = this.props
         const c = contexts[index]
         return (
             <div key={key} style={style} className="ContextsList__context">
@@ -34,52 +36,59 @@ export default function ContextsList({ contexts, onItemPlayClick, currentItem, o
             </div>
         )
     }
-    return (
-        <div className="ContextsList">
-            <AutoSizer>
-                {({ width, height }) => (
-                    <List
-                        className="ContextsList__list"
-                        width={width}
-                        height={height}
-                        rowCount={contexts.length}
-                        rowHeight={getContextHeight}
-                        rowRenderer={_rowRenderer}
-                    />
-                )}
-            </AutoSizer>
-            <ul>
-                {false && contexts.map((c) => (
-                    <li className="context" key={c.context.id}>
-                        <div className="context__cover">
-                            <div className="context__title">
-                                {c.context.title}<br/>
-                                <span className="context__details">{c.items.length} items / </span>
-                                <span className="context__details">{moment.duration(getDuration(c.items), 's').humanize()}</span>
+    componentDidUpdate() {
+        this.refs.AutoSizer.refs.List.recomputeRowHeights()
+    }
+    render() {
+        const { contexts, onItemPlayClick, currentItem, onPlayShowClick, onAddClick } = this.props
+        return (
+            <div className="ContextsList">
+                <AutoSizer ref="AutoSizer">
+                    {({ width, height }) => (
+                        <List
+                            ref="List"
+                            className="ContextsList__list"
+                            width={width}
+                            height={height}
+                            rowCount={contexts.length}
+                            rowHeight={this.getContextHeight.bind(this)}
+                            rowRenderer={this._rowRenderer.bind(this)}
+                        />
+                    )}
+                </AutoSizer>
+                <ul>
+                    {false && contexts.map((c) => (
+                        <li className="context" key={c.context.id}>
+                            <div className="context__cover">
+                                <div className="context__title">
+                                    {c.context.title}<br/>
+                                    <span className="context__details">{c.items.length} items / </span>
+                                    <span className="context__details">{moment.duration(getDuration(c.items), 's').humanize()}</span>
+                                </div>
+                                <div className="context__illustrations">
+                                    {c.items.slice(0, 15).map((i, idx)=> (
+                                        <img src={i.thumbnail} key={idx} onClick={onItemPlayClick.bind(null, i)}/>
+                                    ))}
+                                </div>
                             </div>
-                            <div className="context__illustrations">
-                                {c.items.slice(0, 15).map((i, idx)=> (
-                                    <img src={i.thumbnail} key={idx} onClick={onItemPlayClick.bind(null, i)}/>
+                            <ol>
+                                {c.items.map(i => (
+                                    <li key={i.id}>
+                                        <ListItem
+                                            item={i}
+                                            onPlayClick={onItemPlayClick}
+                                            onAddClick={onAddClick}
+                                            onPlayShowClick={onPlayShowClick}
+                                            isPlaying={currentItem === i}/>
+                                    </li>
                                 ))}
-                            </div>
-                        </div>
-                        <ol>
-                            {c.items.map(i => (
-                                <li key={i.id}>
-                                    <ListItem
-                                        item={i}
-                                        onPlayClick={onItemPlayClick}
-                                        onAddClick={onAddClick}
-                                        onPlayShowClick={onPlayShowClick}
-                                        isPlaying={currentItem === i}/>
-                                </li>
-                            ))}
-                        </ol>
-                    </li>
-                ))}
-            </ul>
-        </div>
-    )
+                            </ol>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
 
 ContextsList.propTypes = {
